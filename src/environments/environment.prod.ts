c47ac1872f03fb94655c5e550bb022795cba3348
GamEGolF <theeramet@builk.com>
2018-10-14 06:31:03 +0700

    add firebase prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDP7P98bRPZt5X4YYEwF1W7J1VOMGA8CRQ",
-    authDomain: "gamegolf-74043.firebaseapp.com",
-    databaseURL: "https://gamegolf-74043.firebaseio.com",
-    projectId: "gamegolf-74043",
-    storageBucket: "gamegolf-74043.appspot.com",
-    messagingSenderId: "99558072242"
+    apiKey: "AIzaSyDVFTPo7qaEO7sgbzkgwHXofvYxx6HjtMg",
+    authDomain: "gamegolf-prod.firebaseapp.com",
+    databaseURL: "https://gamegolf-prod.firebaseio.com",
+    projectId: "gamegolf-prod",
+    storageBucket: "gamegolf-prod.appspot.com",
+    messagingSenderId: "560174108877"
   }
 };
